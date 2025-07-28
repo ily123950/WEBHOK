0316913f1e879594f7ba6f67e7165d2745111b4a
ily123950 <141718416+ily123950@users.noreply.github.com>
2025-07-28 20:13:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const fetch = require("node-fetch");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const webhook = "https://discord.com/api/webhooks/1397128931567603742/ICteuf__9KOTzicVn7lysg7AFbe16q7o2lebabbArWxq-t9bHrfPCbbiVY3zLZTJI9xT";
 
-// Позволяет парсить JSON и формы
-app.use(express.json());
+// Чтобы принимать query-параметры
 app.use(express.urlencoded({ extended: true }));
 
-// GET запрос через query-параметры (?msg=...)
 app.get("/", async (req, res) => {
   const msg = req.query.msg || "Hello from Roblox!";
+  const webhook = "https://discord.com/api/webhooks/1397128931567603742/ICteuf__9KOTzicVn7lysg7AFbe16q7o2lebabbArWxq-t9bHrfPCbbiVY3zLZTJI9xT";
 
   try {
     const response = await fetch(webhook, {
@@ -27,27 +25,6 @@ app.get("/", async (req, res) => {
     res.send("Sent to Discord!");
   } catch (error) {
     res.status(500).send("Error sending to Discord");
-  }
-});
-
-// POST запрос с JSON (embed, content и т.д.)
-app.post("/", async (req, res) => {
-  try {
-    const data = req.body;
-
-    const response = await fetch(webhook, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    });
-
-    if (!response.ok) {
-      return res.status(500).send("Failed to send");
-    }
-
-    res.send("Sent to Discord!");
-  } catch (error) {
-    res.status(400).send("Invalid JSON or error sending to Discord");
   }
 });
 
